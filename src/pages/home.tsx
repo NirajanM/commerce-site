27eaf7c71f7ebf5635f2d2ec5fd123d02b79d673
Nirajan Malla <nirajannitro5@gmail.com>
2022-11-27 10:35:20 +0545

    styling header text - color change

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,7 +9,7 @@ const Home: React.FunctionComponent<IHomeProps> = (props) => {
     return (<div id="home" className="p-4">
         <header className="px-60 py-20 text-center">
             <p id="boldstyle" className="text-5xl text-slate-700">Stickers speaks louder than words, welcome to the city of stickers.</p>
-            <p className="text-slate-500 py-4">feel free to roam around and find the stickers that will stick to your heart first</p>
+            <p className="text-slate-500 py-4">feel free to roam around and find the stickers that will stick to your <span className='text-red-500'>heart</span> first.</p>
             <div className='border-2 border-slate-400/50 rounded-lg px-8 py-2 inline text-center'>
                 <IconButton type="button" aria-label="search">
                     <SearchIcon />
