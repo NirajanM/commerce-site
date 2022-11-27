22563d0c42568e0a4765db183e6a19815b3fb06f
Nirajan Malla <nirajannitro5@gmail.com>
2022-11-27 10:45:57 +0545

    footer edited

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ interface IFooterProps {
 const currentYear: number = new Date().getFullYear();
 const Footer: React.FunctionComponent<IFooterProps> = (props) => {
     return (
-        <footer className='text-xs sm:text-lg bg-blue-600 h-60 text-white flex justify-evenly flex-col items-center'>
+        <footer className='text-xs sm:text-lg bg-blue-600 h-36 text-white flex justify-evenly flex-col items-center'>
             <span>&#60; Designed and Developed by <a className='font-semibold' href="https://www.nirajanmalla.com.np">Nirajan Malla</a> /&#62;</span>
             <span>© {currentYear}, All rights reserved.</span>
         </footer>
