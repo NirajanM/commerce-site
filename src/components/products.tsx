f4f03f4cee23cb749f6e73531046da9271f88e32
Nirajan Malla <nirajannitro5@gmail.com>
2022-11-28 12:53:26 +0545

    changed api call to specific category

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -1,10 +1,51 @@
-import * as React from 'react';
-
+import { useLocation, Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 interface IProductsProps {
 }
+type tProducts = {
+    id: Number;
+    title: String;
+    description: String;
+    price: Number;
+    discountPercentage: Number;
+    rating: Number;
+    stock: Number;
+    brand: String;
+    category: String;
+    thumbnail: String,
+    images: [
+        String
+    ]
+}
+
 
 const Products: React.FunctionComponent<IProductsProps> = (props) => {
-    return (<></>);
+    const location = useLocation();
+    const [products, setProducts] = useState<[] | [tProducts]>([]);
+    const fetchProducts = async () => {
+        try {
+            const res = await axios.get<[tProducts]>(`https://dummyjson.com/products${location.pathname}`);
+            setProducts(res.data);
+            console.log(res.data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    return (
+        <div className='text-slate-600 text-center py-1'>
+            <span className='border-b-2 border-blue-400'>{location.pathname}</span>
+            <div className='flex justify-center py-4'>
+                <Link className='text-white bg-blue-400 hover:bg-blue-600 hover:scale-110 rounded-lg border-2 text-lg px-4 mb-4' to="/">
+                    Go back
+                </Link>
+            </div>
+        </div>);
 };
 
 export default Products;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,92 +5,63 @@ import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 import ShieldIcon from '@mui/icons-material/Shield';
 import HandshakeIcon from '@mui/icons-material/Handshake';
 import { Routes, Route } from 'react-router-dom';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Categories from '../components/categories';
 import Products from '../components/products';
 interface IHomeProps {
 }
-
-type tProducts = {
-    id: Number;
-    title: String;
-    description: String;
-    price: Number;
-    discountPercentage: Number;
-    rating: Number;
-    stock: Number;
-    brand: String;
-    category: String;
-    thumbnail: String,
-    images: [
-        String
-    ]
-}
-
 const Home: React.FunctionComponent<IHomeProps> = (props) => {
-    const [products, setProducts] = useState<null | [tProducts]>(null);
-    const fetchProducts = async () => {
-        try {
-            const res = await axios.get<[tProducts]>(`https://dummyjson.com/products`);
-            setProducts(res.data);
-            console.log(res.data);
-        } catch (err) {
-            console.log(err);
-        }
-    };
-    useEffect(() => {
-        fetchProducts();
-    }, []);
 
-    return (<div id="home" className="py-4">
-        <header className="px-60 py-20 text-center">
-            <p id="boldstyle" className="text-5xl text-slate-700">Who don't love shopping ? and that on wholesale price XD.</p>
-            <p className="text-slate-500 py-4">we are the largest salers of Nepal, feel free to roam around and find the products that will stick to your <span className='text-red-500'>heart</span> first.</p>
-            <div className='border-2 border-slate-400/50 rounded-lg px-8 py-2 inline text-center'>
-                <IconButton type="button" aria-label="search">
-                    <SearchIcon />
-                </IconButton>
-                <InputBase
-                    placeholder="Quick Search"
-                    inputProps={{ 'aria-label': 'Quick Search' }}
-                /></div>
-        </header>
-        <section className='py-40 px-4'>
-            <p className='bg-blue-500 text-white font-semibold text-2xl px-8 py-2 text-center'>Categories:</p>
-            <Routes>
-                <Route path="" element={<Categories />} />
-                <Route path='category/*' element={<Products />} />
-            </Routes>
-        </section>
-        <section className='text-center pb-40 px-4'>
-            <span className='text-blue-500 text-3xl border-b-4 font-bold'>Why Us ?</span>
-            <div className='flex flex-col md:flex-row justify-evenly items-evenly gap-8 md:gap-0 p-12 text-slate-600'>
-                <div className='flex items-center justify-center'>
-                    <LocalShippingIcon fontSize='large' />
-                    <div className='flex flex-col text-left px-4'>
-                        <span className='text-lg'>FAST SHIPPING</span>
-                        <span className='text-slate-400'>All Over Nepal</span>
+    return (
+        <div id="home" className="py-4">
+            <header className="px-60 py-20 text-center">
+                <p id="boldstyle" className="text-5xl text-slate-700">Who don't love shopping ? and that on wholesale price XD.</p>
+                <p className="text-slate-500 py-4">we are the largest salers of Nepal, feel free to roam around and find the products that will stick to your <span className='text-red-500'>heart</span> first.</p>
+                <div className='border-2 border-slate-400/50 rounded-lg px-8 py-2 inline text-center'>
+                    <IconButton type="button" aria-label="search">
+                        <SearchIcon />
+                    </IconButton>
+                    <InputBase
+                        placeholder="Quick Search"
+                        inputProps={{ 'aria-label': 'Quick Search' }}
+                    /></div>
+            </header>
+            <section className='py-40 '>
+                <p className='bg-blue-500 text-white font-semibold text-2xl py-2 text-center'>Categories:</p>
+                <Routes>
+                    <Route path="" element={<Categories />} />
+                    <Route path='category/*' element={<Products />} />
+                </Routes>
+            </section>
+            <section className='text-center pb-40 px-4'>
+                <span className='text-blue-500 text-3xl border-b-4 font-bold'>Why Us ?</span>
+                <div className='flex flex-col md:flex-row justify-evenly items-evenly gap-8 md:gap-0 p-12 text-slate-600'>
+                    <div className='flex items-center justify-center'>
+                        <LocalShippingIcon fontSize='large' />
+                        <div className='flex flex-col text-left px-4'>
+                            <span className='text-lg'>FAST SHIPPING</span>
+                            <span className='text-slate-400'>All Over Nepal</span>
+                        </div>
                     </div>
-                </div>
-                <div className='flex items-center justify-center'>
-                    <ShieldIcon fontSize='large' />
-                    <div className='flex flex-col text-left px-4'>
-                        <span className='text-lg'>TRUST WORTHY</span>
-                        <span className='text-slate-400'>Since 40+ Years</span>
+                    <div className='flex items-center justify-center'>
+                        <ShieldIcon fontSize='large' />
+                        <div className='flex flex-col text-left px-4'>
+                            <span className='text-lg'>TRUST WORTHY</span>
+                            <span className='text-slate-400'>Since 40+ Years</span>
+                        </div>
                     </div>
-                </div>
-                <div className='flex items-center justify-center'>
-                    <HandshakeIcon fontSize='large' />
-                    <div className='flex flex-col text-left px-4'>
-                        <span className='text-lg'>CASH ON DELIVERY</span>
-                        <span className='text-slate-400'>Pay Hand To Hand</span>
+                    <div className='flex items-center justify-center'>
+                        <HandshakeIcon fontSize='large' />
+                        <div className='flex flex-col text-left px-4'>
+                            <span className='text-lg'>CASH ON DELIVERY</span>
+                            <span className='text-slate-400'>Pay Hand To Hand</span>
+                        </div>
                     </div>
-                </div>
 
-            </div>
-        </section>
-    </div>);
+                </div>
+            </section>
+        </div>
+    );
 };
 
 export default Home;
