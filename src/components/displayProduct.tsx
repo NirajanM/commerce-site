34ceba729c061afa10f9f8759ad109f9932e9a7d
Nirajan Malla <nirajannitro5@gmail.com>
2022-11-29 02:48:25 +0545

    small details button

--- src/components/displayProduct.tsx
+++ src/components/displayProduct.tsx
@@ -1,4 +1,3 @@
-
 import { Rating } from '@mui/material';
 interface IDisplayProductProps {
     title: string;
@@ -23,9 +22,8 @@ const DisplayProduct: React.FunctionComponent<IDisplayProductProps> = (props) =>
                 <img className="p-8 rounded-t-lg h-80" src={props.thumbnail} alt="product image" />
             </a>
             <div className="px-5 pb-5">
-                <a href="#">
-                    <h5 className="text-xl font-semibold tracking-tight text-gray-700 dark:text-white">{props.title}</h5>
-                </a>
+                <h5 className="text-xl font-semibold tracking-tight text-gray-700 dark:text-white">{props.title}</h5>
+                <span className='border-b-1 cursor-pointer hover:scale-105 hover:rounded-lg hover:border-2 px-2 py-1 my-4 hover:bg-blue-700 hover:text-white'>check details</span>
                 <div className="flex items-center mt-2.5 mb-5">
                     <Rating name="read-only" value={props.rating} readOnly precision={0.5} />
                     <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">{props.rating}</span>
