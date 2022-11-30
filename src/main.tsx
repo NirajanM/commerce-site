0e6ee73595dfce240bbf9126d949ce7377f32321
Nirajan Malla <nirajannitro5@gmail.com>
2022-11-30 11:24:07 +0545

    login/logout setup with auth0

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-ey7t84cme241e1rf.us.auth0.com"
+      clientId="Kda9AhFuA6DUqcOLBrQ5LLtQ7orxMyD2"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 )
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,23 +7,24 @@ import HandshakeIcon from '@mui/icons-material/Handshake';
 import { Routes, Route } from 'react-router-dom';
 import Categories from '../components/categories';
 import Products from '../components/products';
+import LoginButton from '../components/Login';
 interface IHomeProps {
 }
 const Home: React.FunctionComponent<IHomeProps> = (props) => {
-
     return (
         <div id="home" className="py-4">
             <header className="px-60 py-28 text-center">
                 <p id="boldstyle" className="text-5xl text-slate-700">Who don't love shopping ? and that on wholesale price XD.</p>
                 <p className="text-slate-500 py-4">we are the largest salers of Nepal, feel free to roam around and find the products that will stick to your <span className='text-red-500'>heart</span> first.</p>
-                <div className='border-2 border-slate-400/50 rounded-lg px-8 py-2 inline text-center'>
+                {/* <div className='border-2 border-slate-400/50 rounded-lg px-8 py-2 inline text-center'>
                     <IconButton type="button" aria-label="search">
                         <SearchIcon />
                     </IconButton>
                     <InputBase
                         placeholder="Quick Search"
                         inputProps={{ 'aria-label': 'Quick Search' }}
-                    /></div>
+                    /></div> */}
+                <LoginButton />
             </header>
             <section>
                 <p className='bg-blue-500 text-white font-semibold text-2xl py-2 text-center'>Products Categories:</p>
